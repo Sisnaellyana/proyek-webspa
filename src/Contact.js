a57b517ed5976bd588d776f3ec40c18e8733c1a8
USER DK <USER DK@LAPTOP-696FATGS.com>
2023-06-17 22:05:35 +0700

    Project sisna

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"; 
+  
+class Contact extends Component {   render() {     return ( 
+      <div> 
+        <h2>Contact</h2> 
+        <p>Email: ti-saintek@gmail.com <br /> 
+           Phone: 082345689765
+           WA : 082253887711 
+</p> 
+      </div> 
+    ); 
+  } 
+}   export default 
+Contact; 
